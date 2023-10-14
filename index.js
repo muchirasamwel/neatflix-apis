aaf935d4e3515f13bb20e8cdaed95734775f5bf2
muchirasamwel <muchirasamwel@gmail.com>
2023-10-14 14:49:49 +0300

    * initial setup

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const fs = require('fs')
+
+const app = express()
+
+app.get('/', (req, res) => {
+  
+})
+
+app.listen(3000, () => {
+  console.log('Server ready..')
+})
+
