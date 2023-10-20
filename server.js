d3fea08c926707d7d38da9e369ff92f01479c8bb
muchirasamwel <muchirasamwel@gmail.com>
2023-10-20 10:11:17 +0300

    * Add mongo connection using mongoose

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+const mongoose = require('mongoose')
 const app = require('./app')
+
+mongoose.connect(process.env.DB_CONNECTION).then(con => {
+  console.log('Database connected...')
+})
+
 app.listen(process.env.PORT || 3000, () => {
   console.log('Server ready..')
 })
