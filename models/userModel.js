d55c1c3689fe126c289df9c29df704d6ff9256bc
muchirasamwel <muchirasamwel@gmail.com>
2023-10-20 11:26:16 +0300

    User model * Add user model * Use the user model for User resource operations

---
+++ models/userModel.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
