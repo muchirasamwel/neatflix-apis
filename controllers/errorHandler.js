9818e9b9194a660b567430ef9d2398da3548957b
muchirasamwel <muchirasamwel@gmail.com>
2023-10-31 08:20:52 +0300

    * Send more error info on dev but send only operational erors for production

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -1,8 +1,36 @@
-const errorHandler = (error, req, res, next) => {
-  res.status(error.statusCode ?? 500).json({
+const sendErrorDev = (error, res) => {
+  res.status(error.statusCode).json({
     status: error.status,
-    message: error.message
+    message: error.message,
+    error: error,
+    stack: error.stack
   })
+}
+
+const sendErrorProd = (error, res) => {
+  if (error.isOperational) {
+    res.status(error.statusCode).json({
+      status: error.status,
+      message: error.message
+    })
+  } else {
+    console.log('ERROR ----- ----------', error)
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong..'
+    })
+  }
+}
+
+const errorHandler = (error, req, res, next) => {
+  error.statusCode = error.statusCode ?? 500
+  error.status = error.status ?? 'error'
+
+  if (process.env.NODE_ENV == 'development') {
+    sendErrorDev(error, res)
+  } else {
+    sendErrorProd(error, res)
+  }
 }
 
 module.exports = errorHandler
