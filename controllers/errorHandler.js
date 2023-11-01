aa7e45c9b4d230bd7c61b54cbdc22c8a3f6cfe8e
muchirasamwel <muchirasamwel@gmail.com>
2023-11-01 08:10:46 +0300

    * Handle db duplicate and cast errors

--- controllers/errorHandler.js
+++ controllers/errorHandler.js
@@ -1,3 +1,14 @@
+const AppError = require('../utils/AppError')
+
+const handleCastErrorDB = err => {
+  return new AppError(400, `Invalid ${err.path} with value ${err.value}`)
+}
+
+const handleDuplicateDB = err => {
+  const error = Object.entries(err.keyValue)[0]
+  return new AppError(400, `${error[0]} - ${error[1]}, already exists`)
+}
+
 const sendErrorDev = (error, res) => {
   res.status(error.statusCode).json({
     status: error.status,
@@ -22,7 +33,12 @@ const sendErrorProd = (error, res) => {
   }
 }
 
-const errorHandler = (error, req, res, next) => {
+const errorHandler = (err, req, res, next) => {
+  let error = err
+
+  if (error.code == 11000) error = handleDuplicateDB(error)
+  if (error.name == 'CastError') error = handleCastErrorDB(error)
+
   error.statusCode = error.statusCode ?? 500
   error.status = error.status ?? 'error'
 
